811b96c5d9f741d5e849b31511d764d47fa2ce5a
changheonkim <heon5652@gmail.com>
2022-12-29 22:10:13 +0900

    3-3 button change

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -13,6 +13,8 @@ class MyApp extends StatefulWidget {
 
 class _MyApp extends State<MyApp> {
   var switchValue = false;
+  String test = 'hello';
+  Color _color = Colors.blue;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,15 +26,24 @@ class _MyApp extends State<MyApp> {
       darkTheme: ThemeData.light(),
       home: Scaffold(
           body: Center(
-              child: Switch(
-        value: switchValue,
-        onChanged: (value) {
-          setState(() {
-            print(value);
-            switchValue = value;
-          });
-        },
-      ))),
-    );
-  }
-}
+          child: ElevatedButton(
+          child: Text('$test'),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(_color),
+          ),
+          onPressed: () {
+            if(test == 'hello'){
+              setState((){
+                test = 'flutter';
+                _color = Colors.amber;
+              });
+            } else {
+              setState((){
+                test = 'hello';
+                _color = Colors.blue;
+              });
+            }
+          },
+    ))
+    ));
+}}
